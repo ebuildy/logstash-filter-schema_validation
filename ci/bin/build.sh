097dc75866c731d771b8a41494f776197727ef30
t.decaux <ebuildy@gmail.com>
2018-05-11 14:22:34 +0200

    fix ci

--- ci/bin/build.sh
+++ ci/bin/build.sh
@@ -3,14 +3,9 @@
 set -e
 
 echo "Starting build process in: `pwd`"
-source ./ci/setup.sh
+source ./ci/bin/setup.sh
 
-if [[ -f "ci/run.sh" ]]; then
-    echo "Running custom build script in: `pwd`/ci/run.sh"
-    source ./ci/run.sh
-else
-    echo "Running default build scripts in: `pwd`/ci/build.sh"
-    bundle install
-    bundle exec rake vendor
-    bundle exec rspec spec
-fi
+echo "Running default build scripts in: `pwd`/ci/bin/build.sh"
+bundle install
+bundle exec rake vendor
+bundle exec rspec spec
